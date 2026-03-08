592e9ffb46a4c707c647649f4d0f5c511d1425a6
MARCOS RUIZ GONZALEZ <ruizgonzalezmarcos04@gmail.com>
2026-03-08 19:25:51 +0100

    Migrar back pablo

---
+++ backend/server.js
@@ -0,0 +1,15 @@
+import 'dotenv/config';
+
+import express from 'express';
+import configPipeline from './configExpress/pipeline.js';
+
+const serverExpress = express();
+configPipeline(serverExpress);
+
+serverExpress.listen(3000, (error) => {
+    if (error) {
+        console.log(`error al levantar el servidor express: ${error}`);
+    } else {
+        console.log(`....servidor express levantado y escuchando en el puerto 3000...`);
+    }
+});
